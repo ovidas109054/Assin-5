b924f0b46a3f047a120daa9c294cc4b5976c9cb1
ovidas109054 <ovidas109054@gmail.com>
2026-09-24 20:50:54 +0600

    Stack Updata

--- DevStack-app/src/App.tsx
+++ DevStack-app/src/App.tsx
@@ -4,6 +4,10 @@ import Baner from './Baner'
 import Footer from './Footer'
 import Cards from './Cards'
 import './App.css'
+import Stack from './Stack'
+import type { CardType } from './Types'
+import {ToastContainer,toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const fetchcarddata = async () =>{
   const res = await fetch('/data.json')
@@ -16,23 +20,49 @@ const fetchcarddata = async () =>{
 const CardPromis = fetchcarddata()
 function App() {
   
+const [selectedStack, setSelectedStack] = useState<CardType[]>([])
 
+const addStack = (card:CardType) =>{
+  setSelectedStack((prev) =>{
+    const alreadyadd = prev.some((item) => item.id ===card.id)
+      if(alreadyadd) return prev
+      return[...prev, card]
+  })
+  toast.success(`${card.name} ✓Added to Stack`)
+}
+
+const removetoStack = (id: string) => {
+  setSelectedStack((prev) => prev.filter((item) => item.id !== id))
+}
+
+const removeAll = () =>{
+  setSelectedStack([])
+}
   return (
+
     <><Nav></Nav>
     <Baner></Baner>
     <main>
+      
       <section className='container mx-auto my-10'>
+          <div className=' mb-10'>
+              <h1 className=' text-2xl font-bold '>Explore The <span className='text-fuchsia-600'>Technologies</span></h1>
+              <p className='text-gray-400'>Pick one technology per category to build your ideal stack.</p>
+          </div>
         <div className='grid grid-cols-4 gap-5'>
           <Suspense fallback = {<h2>Loading...</h2>}>
-              <Cards CardPromis = {CardPromis}></Cards> 
+              <Cards CardPromis = {CardPromis} selectedStack = {selectedStack} onAdd = {addStack}></Cards> 
+              <Stack selectedStack = {selectedStack} onRemove = {removetoStack} onRemoveAll = {removeAll}></Stack>
           </Suspense>
-
+        
         </div>
       </section>
     </main>
-
+         
     <Footer></Footer>
+    <ToastContainer position='bottom-right' autoClose={400} aria-label="Notification"/>
     </>
+
   )
 }
 
--- DevStack-app/src/Cards.tsx
+++ DevStack-app/src/Cards.tsx
@@ -3,23 +3,53 @@ import type { CardType } from './Types';
 
 
 
-const Cards = ({CardPromis}:{CardPromis: Promise<CardType[]>}) => {
+const Cards = ({CardPromis,selectedStack,onAdd}:{CardPromis: Promise<CardType[]>; selectedStack: CardType[]; onAdd: (card:CardType) => void;}) => {
     const allCards = use(CardPromis);
     
     return (
-        <div className='container mx-auto items-center mt-20 mb-20'>
-            <div>
-                <h1 className=' text-2xl font-bold '>Explore The <span className='text-fuchsia-600'>Technologies</span></h1>
-                <p className='text-gray-400'>Pick one technology per category to build your ideal stack.</p>
-            </div>
-
-            <div className='col-span-3 grid grid-cols-3 gap-5'>
+         <div className='col-span-3 grid grid-cols-3 gap-5'>
                 {allCards.length === 0 && <p>No Tech Found</p>}
-                {allCards.map((Acards) => (
-                    <p>{Acards.id}</p>
-                ))}
-            </div>
+                {allCards.map((Acards) => {
 
+                    const isAdded = selectedStack.some((item) => item.id === Acards.id);
+                    
+                    return (
+                <div key = {Acards.id} className='bg-white rounded-2xl shadow-md border border-gray-100 p-5 flex flex-col gap-4'>
+                    <div className=' flex items-start justify-between'>
+                        <img src={Acards.icon} alt={Acards.name} className='w-10 h-10 object-contain'/>
+                        <span className='bg-blue-50 text-blue-500 text-[10px] font-medium px-3 py-1 rounded-full'>
+                                {Acards.badge}
+                        </span>
+
+                    </div>
+
+                    <h3 className='text-lg font-bold text-gray-900 text-[15px]'>{Acards.name}</h3>
+                    <p className='text-[8px] text-gray-500 leading-relaxed'>{Acards.description}</p>
+
+                    <div className='flex items-center gap-3 text-sm'>
+                        <span className='bg-gray-100 text-gray-600 px-3 py-1 rounded-full text-[8px]'>
+                            {Acards.category}
+                        </span>
+                        <span className='text-gray-500 text-[8px]'>{Acards.difficulty}</span>
+                        <span className='flex items-center gap-1 text-gray-700 ml-auto'>
+                            
+                            <span className='text-[10px] font-medium'>⭐{Acards.rating}</span>
+                        </span>
+                    </div>
+
+                    <button
+                          onClick={() => onAdd(Acards)}
+                          disabled={isAdded}
+                          className={`w-full text-[12px] font-medium py-3 rounded-2xl transition-colors duration-200 ${isAdded
+                                      ? 'bg-gray-200 text-gray-400 cursor-not-allowed': 'bg-gray-900 text-white hover:bg-gray-800'
+                                       }`}>{isAdded ? 'Added' : 'Add to Stack'}
+                     </button>
+                </div>
+                   
+                );
+                    
+           })}
+          
         </div>
     );
 };
